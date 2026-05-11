37cfcaf6e3cf5c04a6a6dad370f26c8a77aad20a
markvankester <github@accounthub.aleeas.com>
2026-05-11 16:03:43 +0200

    Update for snellius

---
+++ train_sudoku_snellius.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=mdlm_sudoku
+#SBATCH --time=02:00:00           
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=gpu           
+#SBATCH --gres=gpu:1              
+
+# Ensure your PATH is loaded just in case `uv` is installed in ~/.local/bin
+source $HOME/.bashrc
+
+# Navigate to your project directory
+cd $HOME/diffusion-self-correction 
+
+# uv run automatically resolves the virtual environment and runs the script
+uv run MDLM/train_mdlm.py MDLM/configs/train_sudoku.toml
